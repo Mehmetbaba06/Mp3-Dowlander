e5639826368705e2604267ba2a5f4e86db2bbb87
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-05-23 02:48:01 +0300

    Update songs.py

--- JESongBot/plugins/songs.py
+++ JESongBot/plugins/songs.py
@@ -51,7 +51,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '@Infinity_BOTs'
+        rep = '@kanalEfsanestar'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
